bb2866cf7dec62a6b24be44faec9af6fd1d54f0e
jbqueyrie <jbqueyrie@gmail.com>
2015-12-07 15:49:34 +0100

    Ajout de ++

---
+++ src/test/java/org/opencompare/StatisticsPlusPlusTest.java
@@ -0,0 +1,14 @@
+package org.opencompare;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StatisticsPlusPlusTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
